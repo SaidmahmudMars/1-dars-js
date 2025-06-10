c863059bf38cc33af9720ce2059dbb1c4df0d7c5
SaidmahmudMars <abdusamatovsaidmahmud@gmail.com>
2025-06-10 18:22:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+// Funtion turlari
+
+// 1) function declaration
+
+// function botlar(marjona , aziza){
+//     alert(marjona + aziza)
+// }
+// botlar(2,5)
+
+// function kechikdi(){
+// const userName = prompt("Ism")
+//     const newTag = document.createElement("h1");
+//     newTag.textContent = userName;
+//     document.body.appendChild(newTag)
+
+// }
+// kechikdi()
+// kechikdi()
+
+
+
+
+// 2) function expration
+
+
+
+// const mahmudBot = function(){
+//     alert("Habi boxona qilib ol")
+// }
+// mahmudBot()
+
+
+
+// const marjonaBot = () => {
+//     alert("Aziza Uyga vazifa qimagani uchun yuziga ugri chiqqan")
+// }
+// marjonaBot()
